5a6be959ff607a677f690ef9a783fbf265fab299
Pedro Almeida <pedro@abrix.com.br>
2023-10-09 16:47:29 -0300

    ci lab - isolating tests

--- src/examples/counter/counter.exercise.test.tsx
+++ src/examples/counter/counter.exercise.test.tsx
@@ -1,5 +1,8 @@
-import { screen, render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+// @vitest-environment jsdom
+
+// import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import { screen, render } from './test/utilities';
 import Counter from '.';
 
 test('it should render the component', () => {
@@ -9,20 +12,36 @@ test('it should render the component', () => {
 });
 
 test('it should increment when the "Increment" button is pressed', async () => {
-  const user = userEvent.setup();
-  render(<Counter />);
+  // const user = userEvent.setup();
+  const { user } = render(<Counter />);
 
   const currentCount = screen.getByTestId('current-count');
   const incrementButton = screen.getByRole('button', { name: 'Increment' });
-
   await user.click(incrementButton);
-
   expect(currentCount).toHaveTextContent('1');
 });
 
-test.todo('it should render the component with an initial count', () => {});
+test('it should render the component with an initial count', () => {
+  render(<Counter initialCount={4000} />);
+  const currentCount = screen.getByTestId('current-count');
+  expect(currentCount).toHaveTextContent('4000');
+});
 
-test.todo(
+test(
   'it should reset the count when the "Reset" button is pressed',
-  async () => {},
+  async () => {
+    const { user } = render(<Counter initialCount={65} />);
+
+    const currentCount = screen.getByTestId('current-count');
+    const incrementButton = screen.getByRole('button', { name: /increment/i });
+    const resetButton = screen.getByRole('button', { name: /reset/i });
+
+    await user.click(incrementButton);
+    await user.click(incrementButton);
+
+    expect(currentCount).toHaveTextContent('67');
+    
+    await user.click(resetButton);
+    expect(currentCount).toHaveTextContent('0');
+  },
 );
--- src/examples/counter/test/utilities.ts
+++ src/examples/counter/test/utilities.ts
@@ -1,6 +1,8 @@
 import { render as renderComponent } from '@testing-library/react';
 import  userEvent  from '@testing-library/user-event';
 
+export * from '@testing-library/react';
+
 export const render = (ui: React.ReactElement, options?: Parameters<typeof renderComponent>[1]) => {
     // const user = userEvent.setup();
     // const result = renderComponent(ui, options);
